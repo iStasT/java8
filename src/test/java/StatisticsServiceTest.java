6815dfd56a62fcef11fc92b3300ae29411a47f86
Stas Terehin <da.da@bk.ru>
2023-04-01 13:21:25 +0300

    initial commit

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxWhenMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 88, 8, 6, 11, 11, 12};
+        long expected = 88;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
